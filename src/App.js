c50e8335f5c0d21cc4b49a12548b9bfc89aa5169
종인 <uncsvr@gmail.com>
2024-10-16 18:45:25 +0900

    GitHub Pages 라우팅 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -18,7 +18,7 @@ function App() {
   }, [isMenuOpen]);
 
   return (
-    <Router>
+    <Router basename="/whisper-b2b">
       <div className="App">
         <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
         <Routes>
